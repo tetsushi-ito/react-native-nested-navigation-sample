423ac41cf33a86b9dea113b20bdc87c6322e291e
tetsushi <tetsushi0501@gmail.com>
2018-11-09 08:14:15 +0900

    App.tsxを調整

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
 import NavigationService from './navigation/NavigationService';
-import { NavigationActions } from 'react-navigation';
 
 export default class App extends React.Component {
   state = {
@@ -39,13 +38,6 @@ export default class App extends React.Component {
         require('./assets/images/robot-dev.png'),
         require('./assets/images/robot-prod.png'),
       ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        // ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
     ]);
   };
 
